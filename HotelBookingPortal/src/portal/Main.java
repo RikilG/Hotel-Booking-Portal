c367c7ec471f408d3782c2f32b9bce92fdb01db2
Rikil G <rikil.gajarla@gmail.com>
2018-10-28 10:57:41 +0530

    added main(runner) class and customer class

---
+++ HotelBookingPortal/src/portal/Main.java
@@ -0,0 +1,12 @@
+package portal;
+
+public class Main {
+	
+	public static int signInStatus;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		signInStatus = 0;
+	}
+
+}
