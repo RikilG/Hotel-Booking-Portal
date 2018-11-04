5896b1b65a6876869719e9aba3ffc042b53494e3
Rikil G <rikil.gajarla@gmail.com>
2018-11-04 10:15:33 +0530

    Added empty HotelListUI class

---
+++ HotelBookingPortal/src/portal/HotelListUI.java
@@ -0,0 +1,43 @@
+package portal;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class HotelListUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HotelListUI window = new HotelListUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HotelListUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
